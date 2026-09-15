14b5d4a74908cd08bae65dc80850f7b539421baa
Ramon Murilo <chadramon2022@gmail.com>
2026-09-14 22:29:31 -0300

    Configure fct_trips as incremental model with surrogate key; document residual collision rate

--- nyc_taxi/models/marts/fct_trips.sql
+++ nyc_taxi/models/marts/fct_trips.sql
@@ -1,21 +1,33 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='trip_id'
+    )
+}}
+
 with trips as (
 
-    select * from {{ ref('stg_yellow_tripdata') }}
+    select
+        {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_at', 'dropoff_at', 'pickup_location_id', 'dropoff_location_id', 'trip_distance', 'fare_amount']) }} as trip_id,
+        vendor_id,
+        pickup_at,
+        dropoff_at,
+        datediff('minute', pickup_at, dropoff_at)  as trip_duration_minutes,
+        passenger_count,
+        trip_distance,
+        pickup_location_id,
+        dropoff_location_id,
+        payment_type,
+        fare_amount,
+        tip_amount,
+        total_amount
+
+    from {{ ref('stg_yellow_tripdata') }}
+
+    {% if is_incremental() %}
+    where pickup_at > (select max(pickup_at) from {{ this }})
+    {% endif %}
 
 )
 
-select
-    vendor_id,
-    pickup_at,
-    dropoff_at,
-    datediff('minute', pickup_at, dropoff_at)  as trip_duration_minutes,
-    passenger_count,
-    trip_distance,
-    pickup_location_id,
-    dropoff_location_id,
-    payment_type,
-    fare_amount,
-    tip_amount,
-    total_amount
-
-from trips
+select * from trips
